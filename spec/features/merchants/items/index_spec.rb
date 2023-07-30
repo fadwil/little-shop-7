ee17cb009a57dbbb9c18150573382fd3b17dbf42
Parker Boeing <parker.boeing@gmail.com>
2023-07-30 16:44:34 -0600

    test: User story 8 merchant item index page group by item status

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -23,9 +23,34 @@ RSpec.describe "merchants/:merchant_id/items index" do
 
       click_button "Enable"
       expect(current_path).to eq(merchant_items_path(merchant))
-
       expect(page).to have_content("enabled")
       expect(page).to_not have_content("disabled")
+    end
+  end
+
+  it "groups items by item status" do
+    merchant = Merchant.first
+    visit merchant_items_path(merchant)
+    item_1 = merchant.items[0]
+    item_2 = merchant.items[1]
+    item_3 = merchant.items[2]
+    item_4 = merchant.items[3]
+
+    item_1.update(status:"enabled")
+    item_2.update(status:"enabled")
+
+    within "#Enabled" do
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content(item_2.name)
+      expect(page).to_not have_content(item_3.name)
+      expect(page).to_not have_content(item_4.name)
+    end
+
+    within "#Disabled" do
+      expect(page).to have_content(item_3.name)
+      expect(page).to have_content(item_4.name)
+      expect(page).to_not have_content(item_1.name)
+      expect(page).to_not have_content(item_2.name)
     end
   end
 end
